a3b8baa5a871100d4f5d12de65ae6d07348af7bd
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-11-04 15:41:17 +0000

    Update 13 files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { MainComponent } from './Components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
