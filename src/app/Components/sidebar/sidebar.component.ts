b26ba7f45814f98cf00eb6c42b4301b29058bd6d
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-11-05 11:09:48 +0000

    Update 3 files

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  sidebarLinks: string[] = ['Main', 'About', 'Project List'];
+  constructor() {}
+  ngOnInit(): void {}
 }
