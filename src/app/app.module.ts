befee2afbbe789eb54d61496b2c545fcfcf8adde
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-11-06 11:05:47 +0000

    Update tasks.component.html and app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
 import { MainComponent } from './Components/main/main.component';
 import { TasksComponent } from './Components/tasks/tasks.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,13 +17,10 @@ import { TasksComponent } from './Components/tasks/tasks.component';
     FooterComponent,
     SidebarComponent,
     MainComponent,
-    TasksComponent
+    TasksComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
