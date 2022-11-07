fd4324271a9c7f4609519fcbad98a445dab1a91e
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-11-07 19:02:05 +0000

    Update tasks.component.html and lightbox.directive.ts

--- src/app/Directives/lightbox.directive.ts
+++ src/app/Directives/lightbox.directive.ts
@@ -4,13 +4,11 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[LightBox]',
 })
 export class LightBoxDirective {
-  constructor(private elemRef: ElementRef) {
-    this.elemRef.nativeElement.style.color = 'red';
-  }
+  constructor(private elemRef: ElementRef) {}
   @HostListener('mouseenter') onMouseEnter() {
-    this.elemRef.nativeElement.classList.add('bg-danger');
+    this.elemRef.nativeElement.classList.add('text-bg-primary');
   }
   @HostListener('mouseleave') onMouseLeave() {
-    this.elemRef.nativeElement.classList.remove('bg-dark');
+    this.elemRef.nativeElement.classList.remove('text-bg-primary');
   }
 }
