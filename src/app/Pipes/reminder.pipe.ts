2b6b3c8538b94de07c0d82ef004fd40531c12a04
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-11-11 11:43:39 +0000

    modified: ReminderPipe

--- src/app/Pipes/reminder.pipe.ts
+++ src/app/Pipes/reminder.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReminderPipe implements PipeTransform {
   transform(value: number): string {
-    let now:number = Date.now();
+    let now: number = Date.now();
     let remind: number = Math.floor((now - value) / (24 * 60 * 60 * 1000));
-    return remind ? `${remind} day` : 'Today';
+    return remind > 0 ? `${remind} day` : remind === 0 ? 'Today' : 'Pass';
   }
 }
