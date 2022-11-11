75e4700c9767708081d99fa41daf685c5442ed65
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-11-11 12:09:57 +0000

    modified: tasks

--- src/app/Pipes/reminder.pipe.ts
+++ src/app/Pipes/reminder.pipe.ts
@@ -7,6 +7,6 @@ export class ReminderPipe implements PipeTransform {
   transform(value: number): string {
     let now: number = Date.now();
     let remind: number = Math.floor((now - value) / (24 * 60 * 60 * 1000));
-    return remind > 0 ? `${remind} day` : remind === 0 ? 'Today' : 'Pass';
+    return remind > 0 ? `${remind} day` : remind === 0 ? `'Today'` : 'Pass';
   }
 }
